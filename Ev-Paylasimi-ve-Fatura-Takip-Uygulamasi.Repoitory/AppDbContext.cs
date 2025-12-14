951de40e1025c373bcf9bf363a423b8e4222ba32
Barış <100820351+keserbaros1@users.noreply.github.com>
2025-12-14 14:45:43 +0300

    10 - AppDbContent Closes #8

---
+++ Ev-Paylasimi-ve-Fatura-Takip-Uygulamasi.Repoitory/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Ev_Paylasimi_ve_Fatura_Takip_Uygulamasi.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Ev_Paylasimi_ve_Fatura_Takip_Uygulamasi.Repoitory
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext(options)
+    {
+        public DbSet<Expenses> Expenses { get; set; }
+        public DbSet<ExpenseShare> ExpenseShares { get; set; }
+        public DbSet<House> Houses { get; set; }
+        public DbSet<HouseMember> houseMembers { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
