d4a2da175e9d04b63a5685dfd8975a9f5eb99cd6
Barış <100820351+keserbaros1@users.noreply.github.com>
2025-12-15 14:38:18 +0300

    21- Autofac, Repository, Service, Module  Closes #14

---
+++ Ev-Paylasimi-ve-Fatura-Takip-Uygulamasi.API/Modules/RepoServiceModule.cs
@@ -0,0 +1,45 @@
+﻿using Autofac;
+using Ev_Paylasimi_ve_Fatura_Takip_Uygulamasi.Core.Repositories;
+using Ev_Paylasimi_ve_Fatura_Takip_Uygulamasi.Core.Services;
+using Ev_Paylasimi_ve_Fatura_Takip_Uygulamasi.Core.UnitOfWorks;
+using Ev_Paylasimi_ve_Fatura_Takip_Uygulamasi.Repoitory;
+using Ev_Paylasimi_ve_Fatura_Takip_Uygulamasi.Repoitory.Repositories;
+using Ev_Paylasimi_ve_Fatura_Takip_Uygulamasi.Repoitory.UnitOfWorks;
+using Ev_Paylasimi_ve_Fatura_Takip_Uygulamasi.Service.Mappings;
+using Ev_Paylasimi_ve_Fatura_Takip_Uygulamasi.Service.Services;
+using System.Reflection;
+using Module = Autofac.Module;
+
+namespace Ev_Paylasimi_ve_Fatura_Takip_Uygulamasi.API.Modules
+{
+    public class RepoServiceModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterGeneric(typeof(GenericRepository<>))
+                .As(typeof(IGenericRepository<>))
+                .InstancePerLifetimeScope();
+
+            builder.RegisterGeneric(typeof(Service<>))
+                .As(typeof(IService<>))
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<UnitOfWorks>()
+                .As<IUnitOfWorks>();
+
+            // register type token handler
+
+            var apiAssembly = Assembly.GetExecutingAssembly();
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
+            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly)
+                .Where(x => x.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces();
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly)
+                .Where(x => x.Name.EndsWith("Service"))
+                .AsImplementedInterfaces();
+        }
+    }
+}
